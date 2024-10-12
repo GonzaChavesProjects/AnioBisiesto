6c86167b5688356803b6e7174e00d97a70b973a5
GonzaChavesProjects <gonzalochaves92@gmail.com>
2024-10-12 20:53:20 -0300

    Update Program.cs
    prueba modificación lado GitHub

--- AnioBisiesto/Program.cs
+++ AnioBisiesto/Program.cs
@@ -3,6 +3,12 @@
 //Escribe un programa que tome un año introducido por el usuario y diga si es bisiesto o no. Recuerda
 //que los años múltiplos de 4 son bisiestos, excepto aquellos que son múltiplos de 100 y no lo
 //son de 400. Es decir, el año 2000 si es bisiesto, pero no lo son el 1990 ni el 2100.
+//modificacion prueba
+//modificacion 2 lado visual
+//mod3
+//mod4
+//mod5
+//modificacion desde ireneangel
 
 using System;
 
